d06a6893095e1c55aef5d1efa7e1dc5dfaecbaa5
Mariana Sakamoto <43222644+marisakamoto@users.noreply.github.com>
2020-08-19 22:00:01 -0300

    Preparo das seeds

---
+++ database/seeds/pedido_ProdutoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class pedido_ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table()->insert(
+        //     [
+        //         id()->1,
+        //         'name'->'Mariana',
+        //         string('email')->unique();
+        //         timestamp('email_verified_at')->nullable();
+        //         string('password');
+        //         rememberToken();
+        //         timestamps();
+
+        //     ]
+        // )
+    }
+}
